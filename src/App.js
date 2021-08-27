4d622bfaf26a9ab3f07dec5361d124cec5c08e84
Sarkis Markosyan <sqe121@gmail.com>
2021-08-27 12:13:18 +0400

    second commit/ added listeners

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import { CharGridType, data } from "./constants/hardedData";
 
 function App() {
   const [matrix, setMatrix] = useState([[]]);
+  const [isActive, setIsActive] = useState(false);
 
   useEffect(() => {
     setMatrix(data[0].character_grid);
   }, []);
+
+  useEffect(() => {
+    isActive || [...document.getElementsByClassName("row__item")].forEach(el => el.classList.remove("row__item_active"))
+  }, [isActive])
 
   return (
     <div className="App">
@@ -16,8 +21,36 @@ function App() {
         <div
           className="crossword-matrix"
           onMouseDown={(e) => {
-            e.target.classList.toggle("row__item_active");
+            setIsActive(true);
+            const item = e.target;
+            if(item.classList.contains("row__item")) item.classList.add("row__item_active")
           }}
+
+          onMouseUp={(e) => {
+            setIsActive(false);
+            const matr = e.currentTarget;
+            const cell = e.target;
+            console.log('matrica =', matr);
+            console.log('cell =', cell);
+            // [...matr.children].forEach(row => {
+            //   [...row].forEach( cell => cell.classlist.remove("row__item_active"))
+            // })
+          }}
+
+          // onMouseOut={(e) => {
+          //   setIsActive(false);
+          // }}
+          
+          onMouseOver={e => {
+            const cell = e.target;
+            if(cell.classList.contains("row__item"))
+            {if(isActive) {
+              cell.onclick = (ev) => {ev.target.classList.add("row__item_active")}; 
+              cell.classList.toggle("row__item_active")
+            }
+            else cell.onclick = null;}
+          }
+          }
         >
           {matrix.map((row, i) => (
             <div className="row" key={i}>
