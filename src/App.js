fa2849f3e79978b664ac059995bbc54f04f1dae6
xNekrAmanTx <sqe121@gmail.com>
2021-08-30 21:22:45 +0400

    working game logic with coordinates, self-crossing problem solved

--- src/App.js
+++ src/App.js
@@ -1,61 +1,65 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import "./App.css";
 import { CharGridType, data } from "./constants/hardedData";
+import { languages } from "./constants/locale";
 
-
-const Crossword = ({step, setStep}) => {
+const Crossword = ({ step, setStep }) => {
   const [matrix, setMatrix] = useState([[]]);
   const [isActive, setIsActive] = useState(false);
   const [rightTranslations, setRightTranslations] = useState([]);
-  const [userWord, setUserWord] = useState('');
-  const [sourceLanguage, setSourceLanguage] = useState('');
-  const [targetLanguage, setTargetLanguage] = useState('');
-  const [taskWord, setTaskWord] = useState('');
-  
+  // const [userWord, setUserWord] = useState("");
+  const [sourceLanguage, setSourceLanguage] = useState("");
+  const [targetLanguage, setTargetLanguage] = useState("");
+  const [taskWord, setTaskWord] = useState("");
+
+  const [selectedCells, setSelectedCells] = useState([]);
+
+  const userWord = useMemo(() => {
+    return selectedCells.map(([j, _ ,i]) => matrix[i][j]).join('')
+  }, [selectedCells])
+
   const turnOn = useCallback(() => {
-    setIsActive(true)
+    setIsActive(true);
   }, []);
 
   const turnOff = useCallback(() => {
-    setIsActive(false)
+    setIsActive(false);
   }, []);
 
-  const handleMouseDown = (e) => {
+  const handleMouseDown = id => e => {
     turnOn();
     const item = e.target;
-    if (item.classList.contains("row__item")) {
-      item.classList.add("row__item_active");
-      setUserWord(item.textContent);
-    };
-  }
+    setSelectedCells([id]);
+  };
 
-  const handleMouseUp = () => {
+  const checkWord = () => {
     if (isActive) {
-      if (rightTranslations.some(str => userWord === str)) {
+      // if (rightTranslations.some((str) => selectedCells.join(',') === str)) {
+      if (rightTranslations.some((str) => userWord === str)) {
         alert(`YOU GOD DAMN RIGHT! *** ${userWord} *** - Good Job!`);
-        setStep(step => step !== data.length - 1 ? step + 1 : -1);
+        setStep((step) => (step !== data.length - 1 ? step + 1 : -1));
       }
       turnOff();
     }
-  }
+  };
 
-  const handleMouseOver = e => {
-    const cell = e.target;
-    const prevCell = e.relatedTarget;
+  const handleMouseOver = id => e => {
+    // const cell = e.target;
+    // const prevCell = e.relatedTarget;
     if (isActive) {
-      if (cell.classList.contains("row__item_active")) {
-        prevCell?.classList.remove("row__item_active");
-        setUserWord(userWord.slice(0, -1))
-      }
+      if (selectedCells[selectedCells.length - 2] === id) {
+        setSelectedCells(selectedCells.slice(0, -1));
+      } else if (selectedCells.includes(id)) checkWord();
       else {
-        cell.classList.add("row__item_active");
-        setUserWord(userWord + cell.textContent);
+        /* setSelectedCells({[id] : cell.textContent, ...selectedCells}); */
+        setSelectedCells(selectedCells.concat(id));
       }
     }
-  }
+  };
 
   useEffect(() => {
     setMatrix(data[step].character_grid);
+    // setRightTranslations(Object.keys(data[step].word_locations));
     setRightTranslations(Object.values(data[step].word_locations));
     setSourceLanguage(data[step].source_language);
     setTargetLanguage(data[step].target_language);
@@ -63,76 +67,55 @@ const Crossword = ({step, setStep}) => {
   }, [step]);
 
   useEffect(() => {
-    if (!isActive) {
-      setUserWord('');
-      [...document.getElementsByClassName("row__item")].forEach(el => {
-        el.classList.remove("row__item_active");
-      });
-    }
-  }, [isActive])
+    isActive || setSelectedCells([]);
+  }, [isActive]);
 
-  return(
-  <>
-        <h1 className="crossword-number">
-          {`Level ${step + 1}`}
-        </h1>
-        <div
-          className="crossword-matrix"
-          onMouseDown={handleMouseDown}
-          onMouseUp={handleMouseUp}
-          onMouseLeave={handleMouseUp}
-        >
-          {matrix.map((row, i) => (
-            <div
-              className="row"
-              key={i}
-            >
-              {row.map((el, j) => (
-                <span
-                  className="row__item"
-                  key={j}
-                  onMouseOver={handleMouseOver}
-                >
-                  {el}
-                </span>
-              ))}
-            </div>
-          ))}
-        </div>
-        <h2 className="crossword-task">
-          {`Translate word "${taskWord}" from ${sourceLanguage} to ${targetLanguage}   ... <<${rightTranslations.join(`, `)}>>`}
-        </h2>
-        </>
-  )
-}
+  return (
+    <>
+      <h1 className="crossword-number">{`Level ${step + 1}`}</h1>
+      <div
+        className="crossword-matrix"
+        onMouseUp={checkWord}
+        onMouseLeave={checkWord}
+      >
+        {matrix.map((row, i) => (
+          <div className="row" key={i}>
+            {row.map((el, j) => (
+              <span
+                className={`row__item ${
+                  selectedCells.includes(j + "," + i) && "row__item_active"
+                }`}
+                key={j}
+                onMouseDown={handleMouseDown(`${j},${i}`)}
+                onMouseOver={handleMouseOver(`${j},${i}`)}
+              >
+                {el}
+              </span>
+            ))}
+          </div>
+        ))}
+      </div>
+      <h2 className="crossword-task">
+        {`Translate word "${taskWord}" from ${languages[sourceLanguage]} to ${
+          languages[targetLanguage]
+        } <<${rightTranslations.join(`, `)}>>`}
+      </h2>
+    </>
+  );
+};
 
 function App() {
-
   const [step, setStep] = useState(0);
-
-
-  // useEffect(()=>{
-  //   const observer = new MutationObserver(function (mutations) {
-  //     mutations.forEach(function (mutationRecord) {
-  //       console.log(mutationRecord.target);
-  //     });
-  //   });
-  //   observer.observe(document.getElementById('matrix0'), { attributes: true, subtree: true});
-  // },[])
-
 
   return (
     <div className="App">
       <header className="App-header"></header>
       <main className="App-main">
-        {
-        ~step ?
-        <Crossword step={step} setStep={setStep}/>
-        :
-        <h1>
-          You won. Congrats!
-        </h1>
-        }
+        {~step ? (
+          <Crossword step={step} setStep={setStep} />
+        ) : (
+          <h1>You won. Congrats!</h1>
+        )}
       </main>
       <footer className="App-footer"></footer>
     </div>
